c9ea62f7e2f848aff42d496d5199d604ac9af203
Pranav Kulkarni <103885634+KRYPTONITE8@users.noreply.github.com>
2023-05-22 20:50:27 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php 
+    session_start();
+
+    ?>
+<!DOCTYPE html>
+<html>
+<?php $title="Bloodbank | index page"; ?>
+<?php require 'head.php'; ?>
+<body>
+    <style>
+        body{
+            background: url(image/R.jpeg) no-repeat center fixed;
+            background-color: white;
+        }
+        .card{
+            background-color: white;
+        }
+        img{
+            size: 100%;
+        }
+    </style>
+    <?php require 'header.php'; ?>
+
+    <div class="container cont">
+
+   <?php require 'message.php'; ?>
+
+        <div class="row justify-content-center">
+            <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5 col-xs-6 mb-5" style="width: 60%">
+                
+            </div>
+
+
+</body>
+</html>
